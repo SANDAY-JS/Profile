e5fc3616380bb098a837b354f2f78df1e261582b
SANDAY-JS <72862106+SANDAY-JS@users.noreply.github.com>
2020-11-13 22:06:09 +0900

    Refactoring
    refactoring

--- humburger.js
+++ humburger.js
@@ -1,21 +1,31 @@
 export { humbergerBackground, menu, wholePage, headerSection };
 
+// DOM
 const humbergerBackground = document.querySelector(".hamburger-background");
 const menu = document.querySelector(".menu");
 const wholePage = document.querySelector(".whole-page");
 const headerSection = document.querySelector(".header-section");
 
-function toggleNav() {
+// Methods
+const menuOpen = () => {
   // Humburger Click -> add classLists
   humbergerBackground.addEventListener("click", () => {
     wholePage.classList.toggle("transparent");
     headerSection.classList.toggle("click");
   });
+};
+const menuClose = () => {
   // remove classLists when you touch other spaces.
   wholePage.addEventListener("click", () => {
     wholePage.classList.remove("transparent");
     headerSection.classList.remove("click");
   });
+};
+
+// Events
+function toggleNav() {
+  menuOpen();
+  menuClose();
 }
 
 toggleNav();
