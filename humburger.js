4b761be597a42d40f9e1992ec39a0e9c743c2913
SANDAY-JS <72862106+SANDAY-JS@users.noreply.github.com>
2020-12-03 22:44:51 +0900

    Christmas Update
    Page design has been changed to the Christmas color.
    This JS change is for the movements of snowflakes.

--- humburger.js
+++ humburger.js
@@ -1,12 +1,16 @@
 export { humbergerBackground, menu, wholePage, headerSection };
 
-// DOM
+// --------DOMs--------------//
 const humbergerBackground = document.querySelector(".hamburger-background");
 const menu = document.querySelector(".menu");
 const wholePage = document.querySelector(".whole-page");
 const headerSection = document.querySelector(".header-section");
 
-// Methods
+const toPageTop = document.querySelector(".toPageTop");
+
+// -------- Methods --------------//
+
+//--Hamburger--//
 const menuOpen = () => {
   // Humburger Click -> add classLists
   humbergerBackground.addEventListener("click", () => {
@@ -22,10 +26,59 @@ const menuClose = () => {
   });
 };
 
-// Events
+//--To the Top Btn--//
+const addShowClass = () => {
+  if (window.pageYOffset >= 100) {
+    toPageTop.classList.add("show");
+  } else {
+    toPageTop.classList.remove("show");
+  }
+};
+
+const toTheTop = () => {
+  window.scrollTo({
+    top: 90,
+    left: 0,
+    behavior: "smooth",
+  });
+};
+
+// --------  Events --------------//
 function toggleNav() {
   menuOpen();
   menuClose();
 }
 
 toggleNav();
+
+window.addEventListener("scroll", addShowClass);
+toPageTop.addEventListener("click", toTheTop);
+
+// --------- Christmas Special Update-------------//
+const generate = () => {
+  //Generating an ID so the flake can be removed later
+  let id = "id" + Date.now(); //i.e. "id1606322064360"
+
+  //Creating flake to be added to DOM
+  const flake = document.createElement("i");
+  flake.classList.add("flake");
+  flake.classList.add("far");
+  flake.classList.add("fa-snowflake");
+  flake.classList.add(id);
+
+  //Random positioning and animation duration
+  flake.style.left = Math.random() * 100 + "%";
+  flake.style.animationDuration = Math.floor(Math.random() * 5) + 1 + "s";
+
+  //Adding flake to DOM
+  headerSection.appendChild(flake);
+
+  //Remove flake from DOM after 5 seconds
+  setTimeout(function () {
+    let flakeToRemove = document.querySelector(`.${id}`);
+    flakeToRemove.remove();
+  }, 2000);
+};
+
+//Run every 2s
+setInterval(generate, 350);
