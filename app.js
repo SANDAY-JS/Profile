87bd1e0ddfdb75eeb680c7d003e1b26b3ab6f2a2
SANDAY-JS <72862106+SANDAY-JS@users.noreply.github.com>
2020-12-04 09:40:19 +0900

    Problem fixed?

--- app.js
+++ app.js
@@ -1,25 +1,3 @@
-// export { humbergerBackground, menu, wholePage, headerSection, toggleNav };
-
-const humbergerBackground = document.querySelector(".hamburger-background");
-const menu = document.querySelector(".menu");
-const wholePage = document.querySelector(".whole-page");
-const headerSection = document.querySelector(".header-section");
-
-function toggleNav() {
-  // Humburger Click -> add classLists
-  humbergerBackground.addEventListener("click", () => {
-    wholePage.classList.toggle("transparent");
-    headerSection.classList.toggle("click");
-  });
-  // remove classLists when you touch other spaces.
-  wholePage.addEventListener("click", () => {
-    wholePage.classList.remove("transparent");
-    headerSection.classList.remove("click");
-  });
-}
-
-toggleNav();
-
 // Copied
 let slideIndex = 0;
 
